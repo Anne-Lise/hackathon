6991f2d58f2f4d60cd3e62ea01cd1d498d0bf3bc
coudry.a <coudry.a@edocode.co.jp>
2023-12-26 17:03:24 +0900

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
+
+
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('App tests', () => {
+  test('console.log is when button i clicked', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    render(<App />);
+    userEvent.click(screen.getByRole("button", {name: "Click here"}));
+    expect(consoleSpy).toHaveBeenCalledWith('has been clicked');
+  })
+})
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import OpenAI from "openai";
 
 function App() {
+
+
+  const openai = new OpenAI({ apiKey: process.env.REACT_APP_OPENAI_KEY ,  dangerouslyAllowBrowser: true});
+
+  async function main() {
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log("COMPLETION", completion.choices[0]);
+  }
+   main();
+
+  async function assistant() {
+    const myAssistant = await openai.beta.assistants.create({
+      instructions:
+          "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
+      name: "Math Tutor",
+      tools: [{ type: "code_interpreter" }],
+      model: "gpt-4",
+    });
+
+    console.log("ASSISTANT", myAssistant);
+  }
+  assistant();
+
+  async function thread() {
+    const threadMessages = await openai.beta.threads.messages.create(
+        "thread_abc123", // Thread ID
+        { role: "user", content: "How does AI work? Explain it in simple terms." }
+    );
+    console.log("thread",threadMessages);
+  }
+
+  thread();
+
+  async function run() {
+    const run = await openai.beta.threads.runs.create(
+        "thread_abc123", // Thread ID
+        { assistant_id: "asst_abc123" } // Assistant ID
+    );
+
+    console.log("RUN",run);
+  }
+  run();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {console.log("has been clicked")}}>Click here</button>
       </header>
     </div>
   );
