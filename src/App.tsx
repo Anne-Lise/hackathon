d5f9d34288df1c4e1f122daff62aeec83b0f8a7d
Masaki Abe <abe.m@edocode.co.jp>
2023-12-28 08:53:28 +0900

    Update demo display and prompt

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,16 @@ import OpenAI from "openai";
 import "./style.css";
 
 const personaInput = {
-  Persona: {
-    personName: "Tanaka",
-    Age: 21,
-    Gender: { male: "23%", female: "77%" },
-    Income: 100000,
-    Languages: ["English", "Japanese"],
-    Occupations: ["Student", "Part-time cook"],
-    MaritalStatus: "Single",
-  },
-  Summary: "",
+  PersonName: "Tanaka",
+  Age: 21,
+  Gender: { male: "23%", female: "77%" },
+  Income: "100000 JPY",
+  Languages: ["English", "Japanese"],
+  Occupations: ["Student", "Part-time cook"],
+  MaritalStatus: "Single",
 };
 
-const surveyQuestion = [
+const surveyData = [
   {
     project: "project1",
     question: "What is your age",
@@ -30,7 +27,6 @@ const surveyQuestion = [
     answers: ["english", "french", "japanese", "japanese"],
   },
 ];
-// const surveyAnswers1 = ["25", "25", "45"];
 
 export const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
@@ -41,7 +37,6 @@ function App() {
   const [personaOutput, setPersonaOutput] = useState(
     JSON.stringify(personaInput),
   );
-  // const [promptInput, setPromptInput] = useState("");
   const [updatePersona, setUpdatePersona] = useState(false);
   const [displayOutput, setDisplayOutput] = useState(false);
   const [isUpdated, setIsUpdated] = useState(false);
@@ -53,19 +48,29 @@ function App() {
           messages: [
             {
               role: "system",
-              content: `Based on personaInput and surveyQuestion, in a first step, generate a average persona : Replace the existing value of the personaInput Persona field with the average value when possible. One field can have several values like occupations can be teacher and part-time job but age should be an average of all ages.
-              In a second step,if questions or answers from the surveyQuestion do not match the personaInput, summarize it in a short summary (2 sentences) explaining the main character of this average persona and add it to the Summary field of personaInput.
-              For the format, render is as follow: 
-              {Persona: {...}
-              Summary: {...}
-              }
-              Use the following variables to do step 1 and step 2 : 
-            "${JSON.stringify(personaInput)}", 
-            
-            "${JSON.stringify(surveyQuestion)}"`,
+              content: `Based on variables named personaInput and surveyData, generate an updated persona :
+Replace the existing value of the personaInput fields with a new value when possible.
+The new value will be computed from an analysis of surveyData.
+One field can have several values, for example "Occupations" can have "teacher" and "part-time job" as values.
+
+Example:
+  You have surveyData as follows: 
+  {
+    project: "project1",
+    question: "What is your age",
+    answers: ["25", "25", "45"],
+  }
+  As the question is about the age of the people answering, you can update the "Age" field in the persona.
+  Its value could be calculated based on the average of all answers.
+
+For the format, render it as an object with the same structure as personaInput.
+
+Here are the two variables:
+  personaInput: "${JSON.stringify(personaInput)}"
+
+  surveyData: "${JSON.stringify(surveyData)}"`,
             },
           ],
-          // messages: [{ role: "system", content: `You are a persona assistant. Based on the question and possible answers like ${surveyTemplate}, and result answers ${surveyResults},update the persona profile ${personaInput}` }],
           model: "gpt-3.5-turbo-0613",
         });
         console.log("personaOutput", response.choices[0].message.content);
@@ -77,59 +82,7 @@ function App() {
       completion();
     }
   }, [updatePersona]);
-  //   useEffect(() => {
-  //     if (updatePersona) {
-  //       console.log("call to openai");
-  //
-  //       const assistantWrapper = async () => {
-  //         const assistant = await openai.beta.assistants.create({
-  //           instructions: `
-  //
-  //           Based on personalInput and surveyQuestion and surveyAnswers, what is the average persona ? Replace the existing personInput value with average value.
-  //
-  //
-  //
-  // `,
-  //           model: "gpt-3.5-turbo-1106",
-  //         });
-  //
-  //         // Here is a persona definition: "${JSON.stringify(personaInput)}".
-  //         const thread = await openai.beta.threads.create();
-  //
-  //         const message = await openai.beta.threads.messages.create(thread.id, {
-  //           role: "user",
-  //           content: `personaInput: "${JSON.stringify(personaInput)}"
-  //
-  //           surveyAnswers: "${JSON.stringify(surveyAnswers)}"
-  //
-  //           surveyQuestion: "${JSON.stringify(surveyQuestion)}"`,
-  //         });
-  //
-  //         let run = await openai.beta.threads.runs.create(thread.id, {
-  //           assistant_id: assistant.id,
-  //         });
-  //
-  //         // poll run status until it is completed
-  //         while (run.status !== "completed") {
-  //           await new Promise((resolve) => setTimeout(resolve, 1000));
-  //           run = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-  //         }
-  //
-  //         let messages = await openai.beta.threads.messages.list(thread.id);
-  //         console.log("output", messages.data.length);
-  //         console.log(messages.data[0].content[0]);
-  //         if (messages && messages.data[0].content[0]) {
-  //           setPersonaOutput(JSON.stringify(messages.data[0].content[0]));
-  //         }
-  //         return null;
-  //       };
-  //
-  //       assistantWrapper();
-  //     }
-  //   }, [updatePersona]);
   const personaData = JSON.parse(personaOutput);
-  const persona = personaData.Persona;
-  const summary = personaData.Summary;
 
   return (
     <div className="container">
@@ -141,7 +94,7 @@ function App() {
             alt="persona avatar"
           />
           <div>
-            {Object.entries(persona).map(([key, value]) => {
+            {Object.entries(personaInput).map(([key, value]) => {
               let displayValue = "";
               if (typeof value === "object" && value !== null) {
                 displayValue = Object.entries(value)
@@ -164,7 +117,7 @@ function App() {
             <div>SURVEY #1 RESULTS</div>
             <div>
               <div className="survey-question">
-                {surveyQuestion
+                {surveyData
                   .filter((item) => item.project === "project1")
                   .map((item) => {
                     const answers = item.answers
@@ -195,7 +148,7 @@ function App() {
             <div>SURVEY #2 RESULTS</div>
             <div>
               <div className="survey-question">
-                {surveyQuestion
+                {surveyData
                   .filter((item) => item.project === "project2")
                   .map((item) => {
                     const answers = item.answers
@@ -227,7 +180,24 @@ function App() {
           <div>Customized Persona</div>
           <div>
             {displayOutput && isUpdated && (
-              <div className="output-content-wrapper">{summary}</div>
+              <div>
+                {Object.entries(personaData).map(([key, value]) => {
+                  let displayValue = "";
+                  if (typeof value === "object" && value !== null) {
+                    displayValue = Object.entries(value)
+                      .map(([innerKey, innerValue]) => `${innerKey}: ${innerValue}`)
+                      .join(", ");
+                  } else {
+                    displayValue = String(value);
+                  }
+                  return (
+                    <div key={key} className="persona-output-row">
+                      <strong>{key.replace(/([A-Z])/g, " $1").trim()}:</strong>{" "}
+                      {displayValue}
+                    </div>
+                  );
+                })}
+              </div>
             )}
           </div>
         </div>
